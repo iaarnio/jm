6ab15324b4d38aa6073784b19b0abd09bba81f37
Ilari Aarnio <jia@iki.fi>
2015-05-02 22:23:15 +0300

    add tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,131 @@
-// Define gulp before we start
 var gulp = require('gulp');
+//var uglify = require('uglify');
+var $ = require('gulp-load-plugins')({lazy: true});
+var browserSync = require('browser-sync');
+var config = require('./gulp.config.js')();
+var port = process.env.PORT || config.defaultPort;
 
-// Define Sass and the autoprefixer
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
+gulp.task('default', ['sass', 'watch'], function() {
+});
 
-// This is an object which defines paths for the styles.
-// Can add paths for javascript or images for example
-// The folder, files to look for and destination are all required for sass
-var paths = {
+gulp.task('serve-dev', ['inject'], function() {
+  log('serve-dev');
+  debugger;
+  var nodeOptions = {
+    script: config.nodeServer,
+    delayTime: 1,
+    watch: [config.server]
+  };
 
-  styles: {
-    src: './client/app',
-    files: './**/*.scss',
-    dest: './client/app'
+  return $.nodemon(nodeOptions)
+//    .on('restart', function() {
+//      log('nodemon restarting');
+//      reloadBrowserSyncAfterDelay(config.startBrowsersyncDelay);
+//    })
+    .on('start', function() {
+      log('nodemon starting');
+      startBrowserSync();
+    });
+});
+
+gulp.task('sass', function (){
+  return gulp
+    .src(config.styles.files)
+    .pipe($.sass({
+      outputStyle: 'compressed',
+      sourceComments: 'map',
+      includePaths : [config.styles.src]
+    }))
+    .on('error', function(err){
+      displayError(err);
+    })
+    .pipe($.autoprefixer(
+      'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'
+    ))
+    .pipe(gulp.dest(config.styles.dest));
+});
+
+gulp.task('watch', function() {
+  log('Starting to watch .scss changes');
+  gulp.watch(config.styles.files, ['sass'])
+    //.pipe($.plumber())
+    .on('change', function(evt) {
+      log('[watcher] File ' + evt.path.replace(/.*(?=sass)/,'') + ' was ' + evt.type + ', compiling...');
+    });
+});
+
+gulp.task('wiredep', function() {
+  log('Wire up the bower js & css and app js into the index.html');
+  var options = config.getWiredepDefaultOptions();
+  var wiredep = require('wiredep').stream;
+
+  return gulp
+    .src(config.index)
+    .pipe(wiredep(options))
+    .pipe($.inject(gulp.src(config.js)))
+    .pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject', ['wiredep'], function() {  // sass dependency!
+  log('Wire up the app css into the index.html');
+  return gulp
+    .src(config.index)
+    .pipe($.inject(gulp.src(config.css)))
+    .pipe(gulp.dest(config.client));
+});
+
+gulp.task('js', function() {
+  return gulp
+    .src('./client/**/*.js')
+//    .pipe(uglify())
+    .pipe(gulp.dest('./build/'));
+});
+
+
+////////////////////////////////////////////////////////////
+
+function startBrowserSync() {
+  if (browserSync.active) {
+    return;
   }
 
+  log('Starting browser sync on port ' + port);
+
+  var options =  {
+    proxy: 'localhost:' + port,
+    port: 3000,
+    files: [
+      config.client + '**/*.*',
+      '!' + config.client + '**/*.scss',
+      config.temp + '**/*.css'
+      ],
+    ghostMode: {
+      clicks: true,
+      location: false,
+      forms: true,
+      scroll: true
+    },
+    injectChanges: true,
+    logFileChanges: true,
+    logLevel: 'debug',
+    logPrefix: 'gulp-patterns',
+    notify: true,
+    reloadDelay: 1000
+  };
+
+  browserSync(options);
 }
+
+/*function reloadBrowserSyncAfterDelay(delay) {
+  setTimeout(function() {
+    browserSync.notify('reloading now...');
+    browserSync.reload({stream: false});
+  }, delay);
+}*/
 
 // A display error function, to format and make custom errors more uniform
 // Could be combined with gulp-util or npm colors for nicer output
-var displayError = function(error) {
+function displayError(error) {
 
   // Initial building up of the error
   var errorString = '[' + error.plugin + ']';
@@ -36,40 +141,13 @@ var displayError = function(error) {
   // This will output an error like the following:
   // [gulp-sass] error message in file_name on line 1
   console.error(errorString);
+};
+
+/*function clean(path, done) {
+  log('Cleaning ' + path);
+  del(path, done);
+}*/
+
+function log(msg) {
+	$.util.log($.util.colors.blue(msg));
 }
-
-// Setting up the sass task
-gulp.task('sass', function (){
-  // Taking the path from the above object
-  gulp.src(paths.styles.files)
-    // Sass options - make the output compressed and add the source map
-    // Also pull the include path from the paths object
-    .pipe(sass({
-      outputStyle: 'compressed',
-      sourceComments: 'map',
-      includePaths : [paths.styles.src]
-    }))
-    // If there is an error, don't stop compiling but use the custom displayError function
-    .on('error', function(err){
-      displayError(err);
-    })
-    // Pass the compiled sass through the prefixer with defined
-    .pipe(prefix(
-      'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'
-    ))
-    // Funally put the compiled sass into a css file
-    .pipe(gulp.dest(paths.styles.dest))
-});
-
-// This is the default task - which is run when `gulp` is run
-// The tasks passed in as an array are run before the tasks within the function
-gulp.task('default', ['sass'], function() {
-  // Watch the files in the paths object, and when there is a change, fun the functions in the array
-  gulp.watch(paths.styles.files, ['sass'])
-    // Also when there is a change, display what file was changed, only showing the path after the 'sass folder'
-    .on('change', function(evt) {
-      console.log(
-        '[watcher] File ' + evt.path.replace(/.*(?=sass)/,'') + ' was ' + evt.type + ', compiling...'
-      );
-    });
-});
