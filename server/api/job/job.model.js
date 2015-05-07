ebffccfcedf7c81a968140fb90822e803b612f91
Ilari Aarnio <jia@iki.fi>
2015-05-07 18:56:21 +0300

    simplify model

--- server/api/job/job.model.js
+++ server/api/job/job.model.js
@@ -11,35 +11,37 @@
    * Job Schema
    */
   var JobSchema = new Schema({
-  	created: {
-  		type: Date,
-  		default: Date.now
-  	},
-  	title: {
-  		type: String,
-  		default: '',
-  		trim: true,
-  		required: 'Title cannot be blank'
-  	},
-  	employer: {
-  		type: Schema.ObjectId,
-  		ref: 'User',
-  		required: 'Employer is mandatory'
-  	},
-  	start: {
-  		type: Date,
-  		default: Date.now
-  	},
-  	end: {
-  		type: Date,
-  		default: Date.now
-  	},
-  	description: {
-  		type: String,
-  		default: '',
-  		trim: true
-  	},
-  	requirements: [Schema.Types.ObjectId]
+    title: String,
+    employer: String
+//  	created: {
+//  		type: Date,
+//  		default: Date.now
+//  	},
+//  	title: {
+//  		type: String,
+//  		default: '',
+//  		trim: true,
+//  		required: 'Title cannot be blank'
+//  	},
+//  	employer: {
+//  		type: Schema.ObjectId,
+//  		ref: 'User',
+//  		required: 'Employer is mandatory'
+//  	},
+//  	start: {
+//  		type: Date,
+//  		default: Date.now
+//  	},
+//  	end: {
+//  		type: Date,
+//  		default: Date.now
+//  	},
+//  	description: {
+//  		type: String,
+//  		default: '',
+//  		trim: true
+//  	},
+//  	requirements: [Schema.Types.ObjectId]
   });
 
   module.exports = mongoose.model('Job', JobSchema);
