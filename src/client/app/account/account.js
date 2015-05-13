dc62544e15703893550c63463b862c75d4b83fe6
Ilari Aarnio <jia@iki.fi>
2015-05-13 15:38:36 +0300

    partial implementation

---
+++ src/client/app/account/account.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+
+  angular.module('jokumuuApp')
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/login', {
+          templateUrl: 'app/account/login/login.html',
+          controller: 'LoginCtrl',
+          controllerAs: 'vm'
+        })
+        .when('/signup', {
+          templateUrl: 'app/account/signup/signup.html',
+          controller: 'SignupCtrl',
+          controllerAs: 'vm'
+        })
+        .when('/settings', {
+          templateUrl: 'app/account/settings/settings.html',
+          controller: 'SettingsCtrl',
+          controllerAs: 'vm',
+          authenticate: true
+        });
+    });
+})();
---
+++ src/client/app/account/signup/signup.controller.js
@@ -0,0 +1,55 @@
+(function () {
+  'use strict';
+
+  angular.module('jokumuuApp')
+    .controller('SignupCtrl', SignupCtrl);
+
+  SignupCtrl.$inject = ['Auth', '$location', '$window', 'logger'];
+
+  function SignupCtrl(Auth, $location, $window, logger) {
+    var vm = this;
+
+    vm.user = {};
+    vm.errors = {};
+    vm.submitted = false;
+
+    vm.register = register;
+    vm.loginOauth = loginOauth;
+
+    activate();
+
+    function activate() {
+      logger.log('SignupCtrl activated');
+    }
+
+    function register(form) {
+      vm.submitted = true;
+
+      if (form.$valid) {
+        Auth.createUser({
+          name: vm.user.name,
+          email: vm.user.email,
+          password: vm.user.password
+        })
+          .then(function () {
+            // Account created, redirect to home
+            $location.path('/');
+          })
+          .catch(function (err) {
+            err = err.data;
+            vm.errors = {};
+
+            // Update validity of form fields that match the mongoose errors
+            angular.forEach(err.errors, function (error, field) {
+              form[field].$setValidity('mongoose', false);
+              vm.errors[field] = error.message;
+            });
+          });
+      }
+    }
+
+    function loginOauth(provider) {
+      $window.location.href = '/auth/' + provider;
+    }
+  }
+})();
