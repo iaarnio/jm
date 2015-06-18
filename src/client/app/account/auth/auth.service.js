12736de4e9c1026dab2b5cb35e4dc7f8be5a724e
Ilari Aarnio <jia@iki.fi>
2015-06-18 12:28:28 +0300

    remove debug log

--- src/client/app/account/auth/auth.service.js
+++ src/client/app/account/auth/auth.service.js
@@ -110,6 +110,7 @@ angular.module('jokumuuApp')
      * @return {Object} user
      */
     function getCurrentUser() {
+      logger.info('getCurrentUser:' + currentUser);
       return currentUser;
     }
 
