350d4818af0d339d0a68336fe7a27cfd755771bc
Ilari Aarnio <jia@iki.fi>
2015-07-30 15:21:48 +0300

    add sinon, chai

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,5 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="sinon/sinon.d.ts" />
