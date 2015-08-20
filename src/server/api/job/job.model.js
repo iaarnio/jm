1bbae77432e2b13ec0eb6dbc20377acaa48174f9
Ilari Aarnio <jia@iki.fi>
2015-08-20 12:53:26 +0300

    Add new fields

--- src/server/api/job/job.model.js
+++ src/server/api/job/job.model.js
@@ -11,44 +11,51 @@
    * Job Schema
    */
   var JobSchema = new Schema({
-    title: String,
-    employer: String
-//  	created: {
-//  		type: Date,
-//  		default: Date.now
-//  	},
-//  	title: {
-//  		type: String,
-//  		default: '',
-//  		trim: true,
-//  		required: 'Title cannot be blank'
-//  	},
-//  	employer: {
-//  		type: Schema.ObjectId,
-//  		ref: 'User',
-//  		required: 'Employer is mandatory'
-//  	},
-//  	start: {
-//  		type: Date,
-//  		default: Date.now
-//  	},
-//  	end: {
-//  		type: Date,
-//  		default: Date.now
-//  	},
-//  	description: {
-//  		type: String,
-//  		default: '',
-//  		trim: true
-//  	},
+  	created: {
+  		type: Date,
+  		default: Date.now
+  	},
+  	employer: {
+  		type: Schema.ObjectId,
+  		ref: 'User',
+  		required: 'Employer is mandatory'
+  	},
+  	title: {
+  		type: String,
+  		default: '',
+  		trim: true,
+  		required: 'Title cannot be blank'
+  	},
+  	start: {
+  		type: Date,
+  		default: Date.now
+  	},
+  	end: {
+  		type: Date,
+  		default: Date.now
+  	},
+  	address: {
+  		type: String,
+  		default: '',
+  		trim: true
+  	},
+  	lat: {
+  		type: String,
+  		default: '',
+  		trim: true
+  	},
+  	lon: {
+  		type: String,
+  		default: '',
+  		trim: true
+  	},
+  	description: {
+  		type: String,
+  		default: '',
+  		trim: true
+  	}
 //  	requirements: [Schema.Types.ObjectId]
-/*
-    this.category = options.category;
-    this.photoUrls = options.photoUrls;
-    this.tags = options.tags;
-    this.state = options.state;
-    this.location = options.location;
-    */
+
   });
 
   module.exports = mongoose.model('Job', JobSchema);
